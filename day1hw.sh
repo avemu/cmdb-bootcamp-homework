67700bb428fca2d49bc5e92f257c81f6320e8549
avemu <avemu26@gmail.com>
2014-09-02 23:32:27 -0400

    Day1_debug

---
+++ day1hw.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#
+# Day 1 - Homework: Part 2 - debug this bash script
+#
+
+echo "There are around 6 mistakes"
+
+FASTQ_DIR=/Users/cmdb/data/fastq
+OUTPUT_DIR=/Users/cmdb/data/day1
+=SRR072
+
+GENOME_DIR=/Users/cmdb/data/genomes
+=dmel5
+ANNOTATION=dmel-all-r5.57.gff
+
+CORES=4
+
+for i in {1..24} 
+do
+  echo fastqc $FASTQ_DIR/$SAMPLE_PREFIX$i\.fastq.gz -o $OUTPUT_DIR: $i
+  echo tophat: -p4 -G/Users/cmdb/data/day1/dmel-all-r5.57.gff -o/Users/cmdb/data/day1/ --no-novel-juncs --segment-length 20 /Users/cmdb/data/genomes/dmel-all-chromosome-r5.57 /Users/cmdb/data/day1/SRR072893.fastq: $i
+  echo cufflinks: $i
+done

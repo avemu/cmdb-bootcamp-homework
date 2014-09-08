e8e6ed7daf23af99a6bb0a78a771bab20310c1cd
avemu <avemu26@gmail.com>
2014-09-07 22:18:12 -0400

    This is day4 hw

--- day4hw.py
+++ day4hw.py
@@ -32,14 +32,26 @@ chr_sizes ={'2L' :'23011544',
 #transcripts = df[2].str.contains("transcript")
 #print line
 
-name_chrsize = []
+name_plus_chrsize = []
+plus_tss_length=[]
+name_minus_chrsize = []
+minus_tss_length=[]
 while 1:
     line = sys.stdin.readline().split()
     if len(line) == 0:
         break
 #    if line[0] == line[0] in chr_sizes:
     if chr_sizes.has_key(line[0]) and '+' in line and 'transcript' in line:
-        name_chrsize.extend([(line[0], line [3], chr_sizes.get(line[0]))])
-    print name_chrsize
-#    elif "-" in line: 
-#        print line          
+        name_plus_chrsize.extend([(line[0], line [3], chr_sizes.get(line[0]))]
+#        plus_tss_length.extend([()])
+        
+    elif chr_sizes.has_key(line[0]) and '-' in line and 'transcript' in line:
+        name_minus_chrsize.extend([(line[0], line [3], chr_sizes.get(line[0]))])
+    
+
+
+
+
+#Right now will get the gene names, the start of tss and then will get the size of the chr. problem is that it is looping such that it will give me the first line, then the first, second line; then first, second, third lines...
+#Next, I have to write an if loop that will go +250 forward and -250 backward with the tss
+          

b9b60c68eb621d17c65264c06b9159a72867d05e
avemu <avemu26@gmail.com>
2014-09-07 22:21:22 -0400

    This is day2lunch hw

--- day2lunch.py
+++ day2lunch.py
@@ -4,5 +4,40 @@ accepted_hits_fname = "/Users/cmdb/data/day1/accepted_hits.SAM"
 
 f = open (accepted_hits_fname)
 
+nl = 0
+while True: 
+    line = f.readline()
+    if not line: 
+        break
+    else: 
+        nl = nl + 1
+print "Number of alignments " , nl 
 
-print "Number of alignments: ".count [SRR]
+#18417212
+
+t = 0
+while True: 
+    line = f.readline()
+    if "NM:i:1" in line: 
+        t = t + 1
+    else: 
+        break
+print "Number of perfect alignments ", t
+
+u =0
+for line in f:
+    if "NH:i" not in line:
+        break
+    if "NH:i:1" in line:
+        u = u + 1
+print "Number of exactly one location reads", u
+
+for i, line in enumerate(f):
+    fields = line.rstrip ("\r\n"). split("\t")
+    print fields [2]
+
+
+    
+
+
+
